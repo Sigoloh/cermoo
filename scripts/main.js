44af22a6253fbe19bb0b7ceec8b8923a5281ae43
Augusto Sigolo <augustos@pop-os.localdomain>
2022-01-19 23:10:17 -0300

    :rotating_light: Arrumadas algumas coisas do eslint

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
-const tabela = document.getElementById('gameBoard').children[0].children;
 import set from './palavras.js';
 import Updaters from './modules/updaters.js';
 
-if(!localStorage.getItem('statusDoJogador')){
-  localStorage.setItem('statusDoJogador',
+const tabela = document.getElementById('gameBoard').children[0].children;
+
+if (!localStorage.getItem('statusDoJogador')) {
+  localStorage.setItem(
+    'statusDoJogador',
     JSON.stringify(
       {
         sequencia: 0,
         melhorSequencia: 0,
-      }
-    )
+      },
+    ),
   );
 }
 
@@ -17,7 +19,7 @@ const statusDoJogador = JSON.parse(localStorage.getItem('statusDoJogador'));
 
 let tentativa = 0;
 
-let entrada = '';
+const entrada = '';
 
 const updaters = new Updaters(
   statusDoJogador,
@@ -29,53 +31,50 @@ updaters.updateStatus(
   document.getElementById('sequencia'),
 );
 
-const palavraDoDia = set[parseInt(Math.random()*462 + 1)].toUpperCase();
-console.log(palavraDoDia);
+const palavraDoDia = set[parseInt(Math.random() * 462 + 1, 10)].toUpperCase();
+
 const matrizResultado = [];
 
-function submit(){
-  for(let i = 0; i < 5; i++){
-    if(updaters.entrada[i] === palavraDoDia[i]){
+function submit() {
+  for (let i = 0; i < 5; i += 1) {
+    if (updaters.entrada[i] === palavraDoDia[i]) {
       updaters.computaPalavra(i, 'certo', tabela[tentativa]);
-    } else if(palavraDoDia.indexOf(updaters.entrada[i]) == -1){
+    } else if (palavraDoDia.indexOf(updaters.entrada[i]) === -1) {
       updaters.computaPalavra(i, 'erro', tabela[tentativa]);
     } else {
       updaters.computaPalavra(i, 'erroNaPosicao', tabela[tentativa]);
     }
   }
 
-  matrizResultado.push(updaters.getJogada());  
+  matrizResultado.push(updaters.getJogada());
   const ganhou = updaters.entrada.toUpperCase() === palavraDoDia.toUpperCase();
 
   let quadro = '';
 
   const mensagem = document.getElementById('mensagem');
   const painelDoGanhou = document.getElementById('painel');
-  if(ganhou){
-    const tabela = document.getElementById('gameBoard');
-    quadro = updaters.desenhaQuadroNaVitoria(quadro, mensagem, tabela, matrizResultado);
-    console.log(quadro);
+  const tabelaDoJogo = document.getElementById('gameBoard');
+  if (ganhou) {
+    quadro = updaters.desenhaQuadroNaVitoria(quadro, mensagem, tabelaDoJogo, matrizResultado);
+
     statusDoJogador.sequencia += 1;
-    mensagem.innerHTML += `<span> Você ganhou </span>`
+    mensagem.innerHTML += '<span> Você ganhou </span>';
     mensagem.innerHTML += `<div class="tentativas"><p>Resumo das suas tentativas</p>${quadro}</div>`;
-    console.log(mensagem);
   }
-  if(ganhou || tentativa == 5){
+  if (ganhou || tentativa === 5) {
     const links = document.getElementById('links');
     quadro = quadro.replace(/<\/br>/gm, '%0A');
-    links.innerHTML += `<a href="https://twitter.com/intent/tweet?text=${quadro}" class="twitter-share-button" data-text="${quadro}" data-url="http://sigolo.me" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> <br/> <a href="/cermoo/cermoo.html">Jogar Novamente</a>`
+    links.innerHTML += `<a href="https://twitter.com/intent/tweet?text=${quadro}" class="twitter-share-button" data-text="${quadro}" data-url="http://sigolo.me" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> <br/> <a href="/cermoo/cermoo.html">Jogar Novamente</a>`;
   }
-  if(!ganhou && tentativa + 1 > 5){
-    statusDoJogador.melhorSequencia =
-      statusDoJogador.sequencia > statusDoJogador.melhorSequencia ? 
-      statusDoJogador.sequencia :
-      statusDoJogador.melhorSequencia
+  if (!ganhou && tentativa + 1 > 5) {
+    statusDoJogador.melhorSequencia = statusDoJogador.sequencia > statusDoJogador.melhorSequencia
+      ? statusDoJogador.sequencia
+      : statusDoJogador.melhorSequencia;
     statusDoJogador.sequencia = 0;
-    const tabela = document.getElementById('gameBoard');
-    tabela.style="display:none"; 
-    painelDoGanhou.style="display:block";
-    mensagem.innerHTML +="<span> Você perdeu </span>"; 
-    mensagem.innerHTML +=`<p>A palavra era ${palavraDoDia}</p>`
+    tabelaDoJogo.style = 'display:none';
+    painelDoGanhou.style = 'display:block';
+    mensagem.innerHTML += '<span> Você perdeu </span>';
+    mensagem.innerHTML += `<p>A palavra era ${palavraDoDia}</p>`;
   }
   localStorage.setItem('statusDoJogador', JSON.stringify(statusDoJogador));
   updaters.updateStatus(
@@ -83,22 +82,22 @@ function submit(){
     document.getElementById('sequencia'),
   );
   updaters.jogada = [];
-  tentativa++;
+  tentativa += 1;
 }
 
-let posicaoLetra = 0
+let posicaoLetra = 0;
 document.addEventListener('keydown', (event) => {
   const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  if(event.key === 'Enter'){
+  if (event.key === 'Enter') {
     submit(entrada);
     updaters.entrada = '';
     posicaoLetra = 0;
-  }else if(event.key == 'Backspace' && updaters.entrada.length != 0){
+  } else if (event.key === 'Backspace' && updaters.entrada.length !== 0) {
     updaters.entrada = updaters.colocaLetraNaCelula(event.key, posicaoLetra, tabela[tentativa]);
     updaters.entrada = updaters.entrada.substring(0, updaters.entrada.length - 1);
-    posicaoLetra--;
-  } else if(letras.indexOf(event.key) != -1 && updaters.entrada.length != 5){
+    posicaoLetra -= 1;
+  } else if (letras.indexOf(event.key) !== -1 && updaters.entrada.length !== 5) {
     updaters.entrada = updaters.colocaLetraNaCelula(event.key, posicaoLetra, tabela[tentativa]);
-    posicaoLetra++;
+    posicaoLetra += 1;
   }
-})
+});
--- scripts/modules/updaters.js
+++ scripts/modules/updaters.js
@@ -1,11 +1,14 @@
-export default class Updaters{
+export default class Updaters {
   statusDoJogador;
+
   linha;
+
   entrada;
+
   constructor(
     statusDoJogador,
     entrada,
-  ){
+  ) {
     this.statusDoJogador = statusDoJogador;
 
     this.entrada = entrada;
@@ -13,56 +16,77 @@ export default class Updaters{
 
   jogada = [];
 
-  updateStatus(melhorSequencia, sequencia){
+  updateStatus(melhorSequencia, sequencia) {
+    const quadroSequencia = melhorSequencia;
 
-    melhorSequencia.innerHTML = this.statusDoJogador.melhorSequencia;
+    quadroSequencia.innerHTML = this.statusDoJogador.melhorSequencia;
 
-    sequencia.innerHTML = this.statusDoJogador.sequencia;
+    const valorSequencia = sequencia;
+
+    valorSequencia.innerHTML = this.statusDoJogador.sequencia;
   }
 
-  colocaLetraNaCelula( letra, i, linha){
-    if(letra == 'Backspace'){
-      linha.children[i - 1].innerHTML = '';
+  colocaLetraNaCelula(letra, i, linha) {
+    const quadroLinha = linha;
 
-    }else{
+    if (letra === 'Backspace') {
+      quadroLinha.children[i - 1].innerHTML = '';
+    } else {
       this.entrada += letra.toUpperCase();
-      
-      linha.children[i].innerHTML = letra.toUpperCase();
+
+      quadroLinha.children[i].innerHTML = letra.toUpperCase();
     }
     return this.entrada;
   }
 
-  computaPalavra(child, sts, linha){
-    switch (sts){
+  computaPalavra(child, sts, linha) {
+    const quadroLinhaNova = linha;
+
+    switch (sts) {
       case 'erro':
-        linha.children[child].style = 'background: #ca5035';
+        quadroLinhaNova.children[child].style = 'background: #ca5035';
+
         this.jogada.push('&#x1F7E5');
+
         break;
       case 'certo':
-        linha.children[child].style = 'background: #479a32';
+        quadroLinhaNova.children[child].style = 'background: #479a32';
+
         this.jogada.push('&#x1F7E9');
+
         break;
       default:
-        linha.children[child].style = 'background: #adc00f';
+        quadroLinhaNova.children[child].style = 'background: #adc00f';
+
         this.jogada.push('&#x1F7E8');
+
         break;
     }
   }
 
-  getJogada(){
+  getJogada() {
     return this.jogada;
   }
 
-  desenhaQuadroNaVitoria(quadroNaVitoria, painelDoGanhou, tabela, matrizresultado){
-    painelDoGanhou.parentNode.style="display:block";
-    tabela.style="display:none";
-    for(let k = 0; k < matrizresultado.length; k++){
-      for(let h = 0; h < matrizresultado[k].length; h++){
-        quadroNaVitoria += `${matrizresultado[k][h]}`;
-      }
-      quadroNaVitoria += `</br>`;
-    }
-    return quadroNaVitoria;
-  }
+  desenhaQuadroNaVitoria(quadroNaVitoria, painelDoGanhou, tabela, matrizresultado) {
+    const quadroPainelGanhou = painelDoGanhou;
 
+    const tabelaDoQuadroDoPainelDoGanhou = tabela;
+
+    let novoQuadroNaVitoria = quadroNaVitoria;
+
+    quadroPainelGanhou.parentNode.style = 'display:block';
+
+    tabelaDoQuadroDoPainelDoGanhou.style = 'display:none';
+
+    for (let k = 0; k < matrizresultado.length; k += 1) {
+      for (let h = 0; h < matrizresultado[k].length; h += 1) {
+        novoQuadroNaVitoria += `${matrizresultado[k][h]}`;
+      }
+
+      novoQuadroNaVitoria += '</br>';
+    }
+
+    return novoQuadroNaVitoria;
+  }
 }
